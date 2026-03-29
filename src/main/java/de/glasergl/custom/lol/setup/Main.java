4ab6ce623e8fbadbd038c524f1866f0df160d3f2
glasergl <gabriel.glaser@web.de>
2026-03-29 20:17:52 +0200

    ui design

--- src/main/java/de/glasergl/custom/lol/setup/Main.java
+++ src/main/java/de/glasergl/custom/lol/setup/Main.java
@@ -1,10 +1,8 @@
 package de.glasergl.custom.lol.setup;
 
-import java.awt.Font;
 import java.io.IOException;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 import de.glasergl.custom.lol.setup.file.Images;
 import de.glasergl.custom.lol.setup.file.SetupFileIo;
@@ -22,11 +20,6 @@ public final class Main {
     public static void main(final String[] commandLineArguments) throws IOException {
 	final Images images = new Images();
 	final SetupFileIo fileIo = new SetupFileIo();
-	UIManager.put("Label.font", new Font(Font.SANS_SERIF, Font.PLAIN, 15));
-	UIManager.put("Button.font", new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-	UIManager.put("TextField.font", new Font(Font.SANS_SERIF, Font.PLAIN, 13));
-	UIManager.put("TextArea.font", new Font(Font.SANS_SERIF, Font.PLAIN, 13));
-	UIManager.put("CheckBox.font", new Font(Font.SANS_SERIF, Font.PLAIN, 12));
 	SwingUtilities.invokeLater(() -> new CreateFrame(images, fileIo));
     }
 }
