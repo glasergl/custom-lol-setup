ed38f6d8e3b8fbc5c92d21085305957f263d481a
glasergl <gabriel.glaser@web.de>
2026-05-02 18:15:24 +0200

    add uncaught exception handler

--- src/main/java/de/glasergl/custom/lol/setup/Main.java
+++ src/main/java/de/glasergl/custom/lol/setup/Main.java
@@ -1,7 +1,10 @@
 package de.glasergl.custom.lol.setup;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import de.glasergl.custom.lol.setup.file.Images;
@@ -18,6 +21,14 @@ public final class Main {
      * @param commandLineArguments - unused.
      */
     public static void main(final String[] commandLineArguments) throws IOException {
+	Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+	    final StringWriter stringWriter = new StringWriter();
+	    final PrintWriter printWriter = new PrintWriter(stringWriter);
+	    throwable.printStackTrace(printWriter);
+
+	    JOptionPane.showMessageDialog(null, stringWriter.toString(), "Uncaught Exception: Program will Terminate", JOptionPane.ERROR_MESSAGE);
+	    System.exit(1);
+	});
 	final Images images = new Images();
 	final SetupFileIo fileIo = new SetupFileIo();
 	SwingUtilities.invokeLater(() -> new CreateFrame(images, fileIo));
